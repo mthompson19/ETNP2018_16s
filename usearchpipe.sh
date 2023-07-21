f91bb177f89a1eb6675edb5b8ed3ef8f79e00cb5
Madeleine Thompson <93942737+mthompson19@users.noreply.github.com>
2023-07-21 11:42:30 -0400

    usearch pipeline
    Raw sequencing reads were analyzed with USEARCH (v11) to calculate the expected error starting from 50 base pairs (bp) to 250 bp at 10 bp intervals (Edgar, 2010). This guided our decision to trim the forward and reverse reads to 150 bp. A total of 2,374,826 forward and reverse reads were sequenced from 48 samples, 2,287,897 reads were merged, and 1,849,854 merged reads passed quality filtering. The mean merged length was 254 bp. Removing the low-quality tail of raw reads improved the quality and percentage of merged reads. Trimmed forward and reverse reads were merged using USEARCH with the options “-fastq_maxdiffs 10”. Merged reads were quality filtered (with a maximum expected error of 1.0), dereplicated, and denoised using USEARCH to obtain Amplicon Sequence Variants (ASVs). 93.4% of merged reads were mapped to the ASVs to construct an abundance table. The nucleotide sequences of ASVs were imported to QIIME2 v2022-11 (Caporaso et al. 2010) and classified by a Naïve Bayesian Classifier using the SILVA v138 99% identity OTUs from the 515F/806R region as the reference database (Quast et al. 2013).

---
+++ usearchpipe.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+#SBATCH --job-name=usearch16s
+#SBATCH -n 2
+#SBATCH -N 1
+#SBATCH --output usearch16s.out
+#SBATCH --error usearch16s.err
+#SBATCH -p penglab-48core
+
+#Active Qiime
+source activate qiime2-2021.8
+
+# This is a script to run 16S analysis using USEARCH
+
+# Navigate to working directory
+cd /work/mat19/usearchtest 
+
+# Set directory to TMPDIR 
+
+export TMPDIR='/usearchtest' 
+echo $TMPDIR
+
+#Naviage to raw_reads data
+
+#Quality 
+
+echo "Checking the quality of raw reads..."
+
+mkdir ./fastq_eestats
+
+for fq in *.fastq; do 
+
+usearch -fastq_eestats2 $fq -output ./fastq_eestats/$fq -length_cutoffs 50,300,10
+done
+
+#Trim forward and reverse reads to 200bp
+
+echo "Trimming reads to 200bp..."
+for fq in *.fastq; do
+
+NAME=$(basename $fq .fastq)
+usearch -fastx_truncate ${fq} -trunclen 200 -fastqout ${NAME}_200bp.fastq
+done
+
+#Merge reads 
+echo "Merge reads"
+usearch -fastq_mergepairs $fq -relabel @ -fastq_maxdiffs 5 -fastqout ./200bp_merged.fq -report ./merge_200bp_report.txtq
+
+usearch -fastq_mergepairs *R1.fastq -relabel @ -fastq_maxdiffs 10 -fastq_pctid 80 -fastqout ./200bp_merged.fq -report ./merge_200bp_report.txt
+
+#Filter reads
+echo "filter reads"
+usearch -fastq_filter 200bp_merged.fq -fastq_maxee 1.0 -fastq_minlen 150 -fastaout filtered.fa
+
+#Dereplicate reads 
+echo "duplicate reads"
+usearch -fastx_uniques filtered.fa -fastaout uniques.fa -sizeout -relabel Uniq
+
+#Denoise 
+echo "denoise" 
+usearch -unoise3 uniques.fa -zotus zotus.fa
+# or run this
+usearch -cluster_otus uniques.fa -otus otus.fa -relabel Otu
+
+#ZOTU table 
+echo "ZOTU table" 
+usearch -otutab 200bp_merged.fq -zotus zotus.fa -otutabout zotutab.txt -biomout zotutab.json -mapout zmap.txt -notmatched unmapped.fa -dbmatched zotus_with_sizes.fa -sizeout -threads 10
+## go into zotus.fa and remove the methanobacterium and bacillus (you have to merge the ZOTU and Taxonomy in phyloseq to figure out which ZOTU is each 
+### they are contaminants and only found in abundance in Stn3Cast9370m02um 
+
+#Import ZOTU to Qiime
+echo "Import ZOTU to Qiime"
+qiime tools import --input-path zotus.fa --output-path zotus.qza --type 'FeatureData[Sequence]'
+
+
+#Qiime Taxonomy 
+echo "Qiime Taxonomy"
+qiime feature-classifier classify-sklearn \
+ --i-classifier silva-138-99-515-806-nb-classifier.qza \
+ --i-reads zotus.qza \
+ --o-classification zotu_16S_taxonomy.qza
+
+#Export to tsv
+echo "export to tsv"
+qiime tools export --input-path zotu_16S_taxonomy.qza --output-path 16S_taxonomy_tsv
+
+#done
